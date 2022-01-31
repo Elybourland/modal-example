783db46ad9ead402d3b77610beb3809f674cc4a6
Elybourland <JEBourland@yahoo.com>
2022-01-30 17:07:16 -0800

    created greetings components, open() on ngOnInit(), added buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { GreetingsComponent } from './greetings/greetings.component';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'modal-example';
+
+  constructor(private matDialog: MatDialog) {
+
+  }
+
+  // onOpenDialogClick() {
+  //   this.matDialog.open(GreetingsComponent);
+  // }
+
+  ngOnInit() {
+    this.matDialog.open(GreetingsComponent);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialogModule } from '@angular/material/dialog';
+import { GreetingsComponent } from './greetings/greetings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GreetingsComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
